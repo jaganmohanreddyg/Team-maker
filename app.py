53b5866fe99a3463aef12b072cbedb00c9ab1b2d
jaganmohanreddyg <46699799+jaganmohanreddyg@users.noreply.github.com>
2022-03-15 16:43:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 def main():
     put_text("Online Team maker").style("color:#5EBEC4; font-size:30px ;font-weight: bold")
     info = input_group("Group inputs", [
-    input('Enter names in the same manner as shown below', name='txt',placeholder="perlu type chey ra huka EX:{raakhi,shakthi,rabhasa}",style="color:#F92C85;font-weight: bold"),
+    input('Enter names in the same manner as shown below', name='txt',placeholder="EX: name1,name2,name3 ",style="color:#F92C85;font-weight: bold"),
     input('Enter team size', name='ts',style="color:#F92C85;font-weight: bold")])
 
     # txt="jagan,jaggu,anu,lax,narayana,adi,geetha,krishna,krish"
@@ -34,5 +34,5 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--port", type=int, default=8080)
     args = parser.parse_args()
-
+    
     start_server(main, port=args.port)
